e4e61b72f66efdd9c8021897288f98424dc932f1
ashworth-zach <zashworth2@gmail.com>
2019-01-26 17:51:37 -0600

    fixed weird build error and got api call to work

--- ClientApp/app/components/fetchdata/fetchdata.ts
+++ ClientApp/app/components/fetchdata/fetchdata.ts
@@ -1,15 +1,23 @@
-import { HttpClient } from 'aurelia-fetch-client';
+import { HttpClient } from 'aurelia-http-client';
 import { inject } from 'aurelia-framework';
+import { json } from 'aurelia-fetch-client';
 
 @inject(HttpClient)
 export class Fetchdata {
     public pokemon: any;
 
     constructor(http: HttpClient) {
-        http.fetch('api/SampleData/GetAsync')
-            .then(result => result.json() as Promise<any>)
-            .then(data => {
-                this.pokemon = data;
+        http.createRequest('pokemon/1/')
+            .asGet()
+            .withBaseUrl('https://pokeapi.co/api/v2/')
+            .send()
+            .then(result => { 
+                let jsonData=JSON.parse(result.response);
+                this.pokemon = jsonData; 
+                console.log(jsonData);
             });
+        // .then(data => {
+        //     this.pokemon = data;
+        // });
     }
 }
