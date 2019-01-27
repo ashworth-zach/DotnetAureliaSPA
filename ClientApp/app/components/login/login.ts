b1eb6437a369387bf2f6afdcca95ee1db5254967
ashworth-zach <zashworth2@gmail.com>
2019-01-27 12:17:26 -0600

    added forms

--- ClientApp/app/components/login/login.ts
+++ ClientApp/app/components/login/login.ts
@@ -1,2 +1,13 @@
+import { HttpClient } from 'aurelia-http-client';
+import { inject } from 'aurelia-framework';
+
+@inject(HttpClient)
 export class Login {
-}
+    email = '';
+    password = '';
+ 
+    login(http:HttpClient) {
+       var myUser = { email: this.email, password: this.password }
+       console.log(myUser);
+    };
+ }
